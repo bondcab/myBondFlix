05c9ed9e5a608796010eba1386f7655c3bbe7a62
bondcab <136003551+bondcab@users.noreply.github.com>
2023-09-19 16:20:46 +0100

    exercise 2.10

--- passport.js
+++ passport.js
@@ -24,6 +24,13 @@ passport.use(
                             message: 'Incorrect username or password.',
                         });
                     }
+                    // 2.10 - Password validation using user schema model
+                    if (!user.validatePassword(password)) {
+                        console.log('incorrect password');
+                        return callback(null, false, { message: 'Incorrect password.' });
+                    }
+
+
                     console.log('finished');
                     return callback(null, user);
                 })
