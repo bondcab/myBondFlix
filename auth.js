f122729d0a8a8e3a08baa71b169b9e4cee80d4d1
bondcab <136003551+bondcab@users.noreply.github.com>
2023-12-28 11:48:28 +0000

    Update auth.js

--- auth.js
+++ auth.js
@@ -13,11 +13,29 @@ let generateJWTToken = (user) => {
   });
 };
 
-//POST Login
 module.exports = (router) => {
+  /**
+   * @description Register user
+   * @name POST /users?Username=[username]&Password=[password]
+   * @example
+   * Request data format
+   * none
+   * @example
+   * Response data format
+   * [
+   *   {
+   *     "Username": "Tom Smith",
+   *     "Password": "Chocolate22!",
+   *     "Email": "tom.smith@outlook.com",
+   *     "DOB": "01/03/1994",
+   *
+   *   }
+   * ]
+   */
   router.post("/login", (req, res) => {
     console.log("Received login request with data:", req.body);
 
+    // Use local strategy for passport authentication
     passport.authenticate("local", { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
@@ -25,11 +43,13 @@ module.exports = (router) => {
           user: user,
         });
       }
+      // Log in the user and generate a JWT token
       req.login(user, { session: false }, (error) => {
         if (error) {
           res.send(error);
         }
         let token = generateJWTToken(user.toJSON());
+        // Respond with the user details and the generated JWT token
         return res.json({ user, token });
       });
     })(req, res);
